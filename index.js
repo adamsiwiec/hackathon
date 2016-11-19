62d977fe86691416535af39981df622469b03b2e
Adam Siwiec <brickmaster01@gmail.com>
2016-11-19 12:13:26 -0600

    final production

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var hn = require('hacker-news-api');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 
 
@@ -11,51 +12,58 @@ var bodyParser = require('body-parser');
 app.set('port', (process.env.PORT || 3000));
 
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 
 app.use('/', express.static('public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
+app.use(compression());
 
 app.get('/hackathon', function(req, res) {
 
     if (!req.query.keywords) {
         res.json([]);
     } else {
-    var curated = [];
-    var key = req.query.keywords;
-    var keywords = key.split(' ');
-    hn.story().since('past_24h', function(error, data) {
+        var curated = [];
+        var key = req.query.keywords;
+        var keywords = key.split(' ');
+        hn.story().since('past_24h', function(error, data) {
             if (error) throw error;
-        for (var i = 0; i < data.hits.length; i++) {
+            for (var i = 0; i < data.hits.length; i++) {
+                var splitUp = data.hits[i].title.split(' ');
+                for (var k = 0; k < keywords.length; k++) {
+                    if (splitUp.indexOf(keywords[k]) !== -1) {
+                         var temp = [data.hits[i].url, data.hits[i].title];
+                        curated.push(temp);
+                        break;
+                    }
+                    if (data.hits[i].title.toLowerCase().indexOf(keywords[k].toLowerCase()) !== -1) {
+                        var temp = [data.hits[i].url, data.hits[i].title]
+                        curated.push(temp);
+                        break;
 
-             for (var k = 0; k < keywords.length; k++) {
-               if (data.hits[i].title.toLowerCase().indexOf(keywords[k].toLowerCase()) !== -1) {
-                   var temp = [data.hits[i].url, data.hits[i].title]
-                   curated.push(temp);
-                   break;
+                }
 
-           }
+
+                }
+
+            }
+            if (curated.length > 6) {
+                do {
+                    curated.pop()
+
+                } while (curated.length > 6)
 
 
             }
-
-        }
-        if (curated.length > 6) {
-        do {
-            curated.pop()
-
-        } while (curated.length > 6)
-
-
+            res.json(curated);
+            console.log(curated);
+        });
     }
-        res.json(curated);
-        console.log(curated);
-    });
-}
 });
 
 
