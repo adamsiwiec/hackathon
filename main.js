e6101aebccfae47a998963347194823ad7f6f61b
Adam Siwiec <brickmaster01@gmail.com>
2017-07-14 11:55:33 -0500

    fixed typo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-yvar pm2 = require('pm2');
+var pm2 = require('pm2');
 
 var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
 var maxMemory = process.env.WEB_MEMORY || 512;    // " " "
